1bba2aa816e84546136ba31551c405bbc70f840c
subham5566 <166425980+subham5566@users.noreply.github.com>
2024-05-28 00:19:29 +0530

    Update lang_en.js

--- languages/lang_en.js
+++ languages/lang_en.js
@@ -1,28 +1,28 @@
 module.exports = {
 	"BOT_INITIALIZE": "Bot initialized successfully.",
-	"BOT_NAME": "*|_CHAT RANDOMIZER BOT_|*",
+	"BOT_NAME": "*RANDOM CHATING BOT*",
 
-	"COMMANDS_INFO_MSG": "/search - find a partner.\n/next - stop this dialog and find a new partner.\n/stop - stop this dialog.\n/share - send a link to your telegram account to your partner",
+	"COMMANDS_INFO_MSG": "/search - find a partner.\n/next - stop this Chat and find a new partner.\n/stop - stop this dialog.\n/share - send a link to your telegram account to your partner",
 
 	"ERROR_SEND_FILE": "You cannot send files or documents!",
-	"ERRROR_WITHOUT_PARTNER": "You don't have a partner.",
-	"ERROR_WAITING_PARTNER": "You are already waiting for a partner.",
+	"ERRROR_WITHOUT_PARTNER": "You don't have a partner☹️",
+	"ERROR_WAITING_PARTNER": "You are already waiting for a partner💤",
 	"ERROR_CHATTING": "You are already talking to a partner.",
 
 	"FIND_NEW_PARTNER_MSG": "You have stopped the chat to find a new partner.",
 	"FIND_PARTNER_MSG": "Type /search to find a partner.",
 
-	"PARTNER_FOUND_MSG": "¡Partner found!\n\nType /stop to stop chatting with your new partner.",
+	"PARTNER_FOUND_MSG": "😇 PARTNER FOUND ☺️\n\nType /stop to stop chatting with your new partner \n\nType /next to stop this chat and find a new partner. ",
 
-	"SEARCHING_PARTNER_MSG": "Looking for a partner...",
+	"SEARCHING_PARTNER_MSG": "🔎Looking for a partner...🔍",
 	"SHARE_CONTACT_MSG": "`*SHARED CONTACT:* {0}`",
-	"SHARED_CONTACT_MSG": "Your contact has been sent to your partner!",
+	"SHARED_CONTACT_MSG": "⚠️Your contact has been sent to your partner!⚠️",
 
-	"START_MSG": "The chat where you can interact with strangers from all over the world anonymously.",
+	"START_MSG": "❗The chat where you can interact with strangers from all over the world Anonymously 😈",
 	"STOP_PARTNET_SEARCH_MSG": "Type /stop to cancel your partner's search.",
 	"STOPPED_SEARCH_MSG": "You have stopped searching for a partner.",
-	"STOPPED_CHAT_MSG": "You have stopped the dialog.",
-	"STOPPED_CHAT_PARTNER_MSG": "Your partner has stopped the dialog.",
+	"STOPPED_CHAT_MSG": "You have stopped the Chat 😱",
+	"STOPPED_CHAT_PARTNER_MSG": "Your partner has stopped the Chat ☹️",
 
 	"HELP_MSG": "This bot is for chatting with strangers on Telegram.\n\nThe bot can send messages, links, gifs, stickers, photos and audio."
 };
